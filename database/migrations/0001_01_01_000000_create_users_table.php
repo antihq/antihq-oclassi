5229b2b3a16059083a69b71dc3caa18a2c614fda
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-02-15 11:59:52 -0600

    Consolidate bio and profile_photo_path columns into initial users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('bio')->nullable()->after('email');
+            $table->string('profile_photo_path')->nullable()->after('email');
             $table->rememberToken();
             $table->timestamps();
         });
