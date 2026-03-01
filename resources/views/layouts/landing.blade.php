acaa0e1a76d53c0403cbd0d2d19376ea72179e40
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-03-01 12:03:50 -0600

    wip

---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        @include("partials.head")
+    </head>
+    <body class="antialiased bg-[#1C362E] text-[#FFFBEB] bg-[url('../img/pattern.svg')] bg-repeat bg-cover">
+        {{ $slot }}
+
+        @fluxScripts
+    </body>
+</html>
